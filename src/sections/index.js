cc76a603d8b3e978417030971dad9a3713fb19c1
Shahil <sahilaryan9211@gmail.com>
2023-08-27 22:27:45 +0530

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import SuperQualtiy from './SuperQualtiy';
+import Services from './services';
+import SpecialOffer from './specialOffer';
+import CustomerReviews from './CustomerReviews';
+import Subscribe from './Subscribe';
+import Footer from './Footer';
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQualtiy,
+    Services,
+    SpecialOffer,
+    CustomerReviews,
+    Subscribe,
+    Footer,
+}
